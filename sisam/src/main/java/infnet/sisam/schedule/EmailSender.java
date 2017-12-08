f83d4e34c52034c32f6dba151603ce7b79f70f22
Rafael Souza <raffamz@brasilcap.com.br>
2017-12-08 17:59:55 -0200

    Incluído no schedule, lógica para envio de email.

--- sisam/src/main/java/infnet/sisam/schedule/EmailSender.java
+++ sisam/src/main/java/infnet/sisam/schedule/EmailSender.java
@@ -2,6 +2,7 @@ package infnet.sisam.schedule;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
@@ -10,13 +11,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import infnet.sisam.dao.AvaliacaoDao;
+import infnet.sisam.model.Aluno;
+import infnet.sisam.model.Avaliacao;
 
 @Component
 public class EmailSender {
 
 	@Autowired
 	private MailSender sender;
-	
+
 	@Autowired
 	private AvaliacaoDao avaliacaoDao;
 
@@ -24,16 +27,43 @@ public class EmailSender {
 	public void init() {
 		SimpleDateFormat sdfDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		System.out.println("Scheduler acionado às :" + sdfDate.format(new Date()));
-		
-		avaliacaoDao.findAll();
-		// verifica quem enviaremos email
-		System.out.println("Email enviado para o aluno: xxx");
-		SimpleMailMessage s = new SimpleMailMessage();
-		s.setFrom("avaliacaoinfnettc@gmail.com");
-		s.setTo("avaliacaoinfnettc@gmail.com");//s.setTo("raffamz@gmail.com");
-		s.setText("Olá");
-		sender.send(s);
-		//se enviou email, ifnrom
-		// LOG.debug("Término leitura Fila: " + sdfDate.format(new Date()));
+		Date sysdate = new Date();
+		List<Avaliacao> avaliacoes = avaliacaoDao.findAll();
+		if (!avaliacoes.isEmpty()) {
+			verificaAlunosParaSeremNotificados(sysdate, avaliacoes);
+		} else {
+			System.out.println("Não existem avaliações para envio de notificaão.");
+		}
+
+	}
+
+	private void verificaAlunosParaSeremNotificados(Date sysdate, List<Avaliacao> avaliacoes) {
+		for (Avaliacao avaliacao : avaliacoes) {
+			if (!avaliacao.getDataInicio().before(sysdate)) {
+				if (avaliacao.getTurma() != null) {
+					List<Aluno> alunosParticipantes = avaliacao.getTurma().getAlunos();
+					envioNotificacao(avaliacao, alunosParticipantes);
+				} else {
+					System.out.println("Não existe turma para a avaliação: " + avaliacao.getId());
+				}
+			} else {
+				System.out.println("Avaliação " + avaliacao.getId() + " ainda não deve ser executada.");
+			}
+		}
+	}
+
+	private void envioNotificacao(Avaliacao avaliacao, List<Aluno> alunosParticipantes) {
+		for (Aluno aluno : alunosParticipantes) {
+			System.out.println("Email enviado para o aluno: " + aluno.getNome());
+			SimpleMailMessage s = new SimpleMailMessage();
+			s.setFrom("avaliacaoinfnettc@gmail.com");
+			s.setTo(aluno.getEmail());
+			s.setText(avaliacao.getTextoEmail());
+			try {
+				sender.send(s);
+			} catch (Exception e) {
+				System.out.println("Ocorreu um erro ao enviar email para: " + aluno.getEmail());
+			}
+		}
 	}
 }
