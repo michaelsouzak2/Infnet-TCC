0f915de5c802941d3b0db749a154c58f1256cb7d
MichaelSantos <MichaelSantos@10.50.20.9>
2017-12-17 18:30:57 -0200

    Inclusão de detalhe.jsp para avaliação e remoção de detalhe1.jsp.

--- sisam/src/main/java/infnet/sisam/controller/AvaliacaoController.java
+++ sisam/src/main/java/infnet/sisam/controller/AvaliacaoController.java
@@ -80,7 +80,7 @@ public class AvaliacaoController {
 	
 	@RequestMapping("/buscar/{id}")
 	public ModelAndView buscar(@PathVariable Integer id) {
-		ModelAndView modelAndView = new ModelAndView("avaliacoes/detalhe1");
+		ModelAndView modelAndView = new ModelAndView("avaliacoes/detalhe");
 		Avaliacao avaliacao = avaliacaoService.buscar(id);
 		List<Questionario> questionarios = questionarioService.lista();
 		modelAndView.addObject("avaliacao", avaliacao)
